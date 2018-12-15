66c046eeed9eb99894890294239a141379618063
pichugin-ms <TorusHelm@list.ru>
2018-12-15 17:18:54 +0500

    save

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,53 @@
 "use strict";
+
+$(document).ready(function(){
+    $('#js_menuBtn').on('click', function(){
+        if($('[menuState]').attr('menuState') === 'open') {
+            $('[menuState]').attr('menuState', 'close');
+        } else {
+            $('[menuState]').attr('menuState', 'open');
+        }
+    });
+    $(".js_anchor").click(function() {
+        $('[menuState]').attr('menuState', 'close');
+        var elementClick = $(this).attr("href")
+        var destination = $(elementClick).offset().top - 49;
+        jQuery("html, body").animate({
+            scrollTop: destination
+        }, 800);
+        return false;
+    });
+});
+
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [59.986821, 30.321258],
+            zoom: 16
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Собственный значок метки',
+            balloonContent: 'ООО "Регент Балтика"'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: '../img/placeholderBig.png'
+            // Размеры метки.
+            // iconImageSize: [30, 42],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            // iconImageOffset: [55, -22]
+        });
+
+    myMap.geoObjects
+        .add(myPlacemark)
+});
