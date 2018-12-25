740d555aeb35c1e1525c25fb34f0ef7d4985f645
pichugin-ms <pichugin-ms@galagodigital.ru>
2018-12-25 13:30:46 +0500

    first

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,33 @@
-"use strict";
+// function stylingSelects(selector) {
+//   var stylingSelects = document.querySelectorAll(selector);
+//
+//   if (stylingSelects) {
+//     for (var i = 0; i < stylingSelects.length; i++) {
+//       new CustomSelect({
+//         elem: stylingSelects[i] // id of the original select element
+//       });
+//     }
+//   }
+// }
+
+function openMobileMenu(){
+  var btn = $('.js-mbl-btn'),
+      menu = $('#mobile-menu'),
+      closeBtn = $('.js-close-btn');
+
+  btn.on('click', function(){
+    menu.addClass('is-open');
+  });
+
+  closeBtn.on('click', function(){
+    menu.removeClass('is-open');
+  })
+}
+
+$(document).ready(function(){
+  openMobileMenu();
+});
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   stylingSelects('select');
+// });
